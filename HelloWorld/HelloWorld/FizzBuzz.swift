dcb60023497fbf5398a627edaee757a0b339d6e7
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-14 15:17:54 +0900

    fizzBuzz 함수 추가

--- HelloWorld/HelloWorld/FizzBuzz.swift
+++ HelloWorld/HelloWorld/FizzBuzz.swift
@@ -7,5 +7,14 @@
 
 
 public func fizzBuzz(_ number: Int) -> String {
-    return ""
+    if number % 3 == 0 && number % 5 == 0 {
+        return "fizz-buzz"
+    } else if number % 3 == 0 {
+        return "fizz"
+    } else if number % 5 == 0 {
+        return "buzz"
+    } else {
+        return "\(number)"
+    }
 }
+
