19d73c59a48e0c4019e5bb6bb4e3bf1f44e299bf
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-15 13:13:30 +0900

    Act, Assert 코드 누락 수정

--- Albertos/AlbertosTests/MenuGroupingTests.swift
+++ Albertos/AlbertosTests/MenuGroupingTests.swift
@@ -18,10 +18,17 @@ final class MenuGroupingTests: XCTestCase {
     
     // 카테고리가 하나인 경우 섹션도 하나여야 한다.
     func testMenuWithOneCategoryReturnsOneSection() {
+        // Arrange: 카테고리가 하나여야 한다.
         let menu = [
             MenuItem(category: "pastas"),
             MenuItem(category: "pastas")
         ]
+        
+        // Act
+        let sections = groupMenuByCategory(menu)
+        
+        // Assert
+        XCTAssertEqual(sections.count, 1)
     }
     
     // 메뉴가 비어있으면 섹션도 비어있어야 한다.
