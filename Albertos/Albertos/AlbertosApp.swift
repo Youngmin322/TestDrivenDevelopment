be748842e1fd6d35a7685590bade62bf1729e107
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-15 11:35:45 +0900

    Albertos 프로젝트 파일 생성

---
+++ Albertos/Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by 조영민 on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
