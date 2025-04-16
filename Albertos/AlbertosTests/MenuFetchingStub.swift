864a7e3cfda25b11902d6e8e136ade6dec23b17f
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-16 13:22:02 +0900

    MenuFetchingStub 클래스를 추가하여 MenuListViewModelTests에서 메뉴를 성공적으로 가져오는 테스트를 구현

---
+++ Albertos/AlbertosTests/MenuFetchingStub.swift
@@ -0,0 +1,16 @@
+//
+//  MenuFetchingStub.swift
+//  AlbertosTests
+//
+//  Created by 조영민 on 4/16/25.
+//
+
+import Testing
+
+struct MenuFetchingStub {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
