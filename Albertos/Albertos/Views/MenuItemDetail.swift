667c0bbd2a31be8be8d0f161b5448467091d267f
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-18 10:33:26 +0900

    주문 상세 화면의 UI 구현 및 상태 관리 로직 추가

---
+++ Albertos/Albertos/Views/MenuItemDetail.swift
@@ -0,0 +1,91 @@
+//
+//  MenuItemDetail.swift
+//  Albertos
+//
+//  Created by 조영민 on 4/17/25.
+//
+
+import SwiftUI
+import Combine
+
+struct MenuItemDetail: View {
+    @StateObject var viewModel: ViewModel
+    
+    var body: some View {
+        VStack {
+            Text(viewModel.item.name)
+                .font(.largeTitle)
+                .padding()
+            
+            Text(viewModel.item.category)
+                .padding()
+            
+            Text(viewModel.item.spicy ? "🌶️" : "")
+            
+            Text("$\(viewModel.item.price, specifier: "%.2f")")
+                .font(.title)
+                .padding()
+            
+            Spacer()
+            
+            Button(action: {
+                viewModel.addOrRemoveFromOrder()
+            }) {
+                Text(viewModel.orderButtonText)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+            .navigationTitle("\(viewModel.item.name) 상세")
+        }
+    }
+}
+
+extension MenuItemDetail {
+    class ViewModel: ObservableObject {
+        let item: MenuItem
+        let orderController: OrderController
+        
+        @Published private(set) var orderButtonText: String = "주문 삭제"
+        
+        private var cancellables = Set<AnyCancellable>()
+        
+        init(item: MenuItem, orderController: OrderController = OrderController()) {
+            self.item = item
+            self.orderController = orderController
+            setupOrderButtonPublisher()
+        }
+        
+        func setupOrderButtonPublisher() {
+            self.orderController.$order.sink { [weak self] order in
+                guard let self else { return }
+                if order.items.contains(self.item) {
+                    self.orderButtonText = "주문 삭제"
+                } else {
+                    self.orderButtonText = "주문 추가"
+                }
+            }
+            .store(in: &cancellables)
+        }
+        
+        func addOrRemoveFromOrder() {
+            if orderController.isItemInOrder(item) {
+                orderController.removeFromOrder(item)
+            } else {
+                orderController.addToOrder(item)
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuItemDetail(
+        viewModel: MenuItemDetail.ViewModel(
+            item: menu[0],
+            orderController: OrderController()
+        )
+    )
+}
--- Albertos/Albertos/Views/OrderDetail.swift
+++ Albertos/Albertos/Views/OrderDetail.swift
@@ -5,8 +5,8 @@
 //  Created by 조영민 on 4/18/25.
 //
 
-
 import SwiftUI
+import Combine
 
 struct OrderDetail: View {
     @StateObject private var viewModel: ViewModel
@@ -16,12 +16,23 @@ struct OrderDetail: View {
     }
     
     var body: some View {
-        NavigationStack {
-            List {
-                // TODO: 주문 목록을 보여주는 UI
+        List {
+            ForEach(viewModel.orderItems) { item in
+                HStack {
+                    Text(item.name)
+                        .font(.headline)
+                    Spacer()
+                    Text("$\(item.price, specifier: "%.2f")")
+                        .foregroundColor(.gray)
+                }
             }
-            .navigationTitle("주문 상세")
+            
+            Section(header: Text("총 가격")) {
+                Text("$\(viewModel.totalPrice, specifier: "%.2f")")
+                    .font(.headline)
+            }
         }
+        .navigationTitle("주문 상세")
     }
 }
 
@@ -29,8 +40,25 @@ extension OrderDetail {
     class ViewModel: ObservableObject {
         let orderController: OrderController
         
+        @Published private(set) var orderItems: [MenuItem] = []
+        @Published private(set) var totalPrice: Double = 0.0
+        
+        private var cancellables = Set<AnyCancellable>()
+        
         init(orderController: OrderController) {
             self.orderController = orderController
+            
+            setupSubscriptions()
+        }
+        
+        private func setupSubscriptions() {
+            orderController.$order
+                .sink { [weak self] order in
+                    guard let self = self else { return }
+                    self.orderItems = order.items
+                    self.totalPrice = order.items.reduce(0) { $0 + $1.price }
+                }
+                .store(in: &cancellables)
         }
     }
 }
