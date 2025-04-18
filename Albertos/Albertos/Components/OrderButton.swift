7149a8cf329a9233380758f86e75305344a464b2
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-18 10:27:10 +0900

    폴더 정리

---
+++ Albertos/Albertos/Components/OrderButton.swift
@@ -0,0 +1,58 @@
+//
+//  OrderButton.swift
+//  Albertos
+//
+//  Created by 조영민 on 4/18/25.
+//
+
+import SwiftUI
+import Combine
+
+struct OrderButton: View {
+    @StateObject private var viewModel: ViewModel
+    
+    init(orderController: OrderController) {
+        _viewModel = StateObject(wrappedValue: ViewModel(orderController: orderController))
+    }
+    
+    var body: some View {
+        Button(action: {
+            // 주문 상세 화면으로 이동하는 로직
+            viewModel.showOrderDetail.toggle()
+        }) {
+            Text(viewModel.buttonText)
+        }
+        .sheet(isPresented: $viewModel.showOrderDetail) {
+            OrderDetail(orderController: viewModel.orderController)
+        }
+    }
+}
+
+extension OrderButton {
+    class ViewModel: ObservableObject {
+        @Published var buttonText: String = "주문하기"
+        @Published var showOrderDetail: Bool = false
+        
+        let orderController: OrderController
+        
+        private var cancellables = Set<AnyCancellable>()
+        
+        init(orderController: OrderController) {
+            self.orderController = orderController
+            setupOrderButtonPublisher()
+        }
+        
+        private func setupOrderButtonPublisher() {
+            orderController.$order
+                .sink { [weak self] order in
+                    guard let self = self else { return }
+                    if order.items.isEmpty {
+                        self.buttonText = "주문 목록"
+                    } else {
+                        self.buttonText = "주문 목록 \(order.items.count)"
+                    }
+                }
+                .store(in: &cancellables)
+        }
+    }
+}
